e7641315105d95fec7920fcc9c485e7bd71cf3eb
luthfimubarok71 <luthfimubarok71@gmail.com>
2025-10-30 16:40:54 +0700

    Reduce user factory seeding from 15 to 3 for testing purposes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(15)->create();
+        User::factory(3)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
