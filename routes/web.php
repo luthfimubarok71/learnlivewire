8e59c7d3ce6068d911e0874cd0c613ba1d6ec487
luthfimubarok71 <luthfimubarok71@gmail.com>
2025-10-30 17:49:32 +0700

    Refactor routes to use Livewire components for home, users, and about pages

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Livewire\Home;
+use App\Livewire\About;
+use App\Livewire\Users;
 use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/users', function () {
-    return view('users');
-});
+Route::get('/', Home::class);
+Route::get('/counter', Counter::class);
+Route::get('/users', Users::class);
+Route::get('/about', About::class);
