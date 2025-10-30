919e9671991aba9ff50f17a7214a2b9f7b1b9827
luthfimubarok71 <luthfimubarok71@gmail.com>
2025-10-30 17:50:09 +0700

    Refactor Users component by removing unused properties and methods, and simplify the view structure in users.blade.php

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -2,67 +2,8 @@
 
 namespace App\Livewire;
 
-use App\Models\User;
 use Livewire\Component;
-use Illuminate\Support\Facades\Hash;
-use Livewire\Attributes\Validate;
-use Livewire\Features\SupportFileUploads\WithFileUploads;
-use Livewire\WithPagination;
+use Livewire\Attributes\Title;
 
-class Users extends Component
-{
-    use WithFileUploads;
-    use WithPagination;
-
-    #[Validate('required|min:3')]
-    public $name;
-
-    #[Validate('required|email:dns|unique:users,email')]
-    public $email;
-
-    #[Validate('required|min:3')]
-    public $password;
-
-    #[Validate('image|max:5120')]
-    public $avatar;
-
-    public $query = '';
-
-    public function updatingQuery()
-    {
-        $this->resetPage();
-    }
-    public function search()
-    {
-        $this->resetPage();
-    }
-
-    public function createNewUser()
-    {
-        $validated = $this->validate();
-
-        if ($this->avatar) {
-            $validated['avatar'] = $this->avatar->store('avatars', 'public');
-        }
-
-        User::create([
-            'name' => $this->name,
-            'email' => $this->email,
-            'password' => Hash::make($this->password),
-            'avatar' => $validated['avatar']
-        ]);
-
-        $this->reset();
-        session()->flash('success', 'User created successfully!');
-    }
-
-    public function render()
-    {
-        return view('livewire.users', [
-            'title' => 'Users Page',
-            'users' => User::latest()
-                ->where('name', 'like', '%' . $this->query . '%')
-                ->paginate(6),
-        ]);
-    }
-}
+#[Title('Users Page')]
+class Users extends Component {}
