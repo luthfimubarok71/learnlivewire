cb207c404dd7f30839ffaf1362f23e37fac2de3a
luthfimubarok71 <luthfimubarok71@gmail.com>
2025-10-30 11:40:32 +0700

    Add user creation functionality and update user list display

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -8,6 +8,15 @@ use Livewire\Component;
 class Users extends Component
 {
 
+    public function createUser()
+    {
+        User::create([
+            'name' => 'User ' . rand(1, 1000),
+            'email' => 'user' . rand(1, 1000) . '@example.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.users', [
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,4 +1,14 @@
-<div>
+<div class="w-1/2 m-auto my-10">
     <h1 class="text-3xl font-semibold">{{ $title }}</h1>
     <p>Users Counter : {{ count($users) }}</p>
+    <button wire:click="createUser"
+        class="text-white bg-blue-500 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 my-2 cursor-pointer">Create
+        User</button>
+    <hr class="my-4 border-l ">
+    <h2 class="text-2xl font-semibold mb-2">User Lists</h2>
+    <ul class="list-disc list-inside">
+        @foreach ($users as $user)
+            <li>{{ $user->name }} - {{ $user->email }}</li>
+        @endforeach
+    </ul>
 </div>
