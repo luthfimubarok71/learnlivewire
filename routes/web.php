b959dcce0787c3a9752ffef9664b0f2035f22562
luthfimubarok71 <luthfimubarok71@gmail.com>
2025-11-01 22:26:01 +0700

    Add Contacts Livewire component route to web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Livewire\Home;
 use App\Livewire\About;
+use App\Livewire\Contacts;
 use App\Livewire\Users;
 use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,4 @@ Route::get('/', Home::class);
 Route::get('/counter', Counter::class);
 Route::get('/users', Users::class);
 Route::get('/about', About::class);
+Route::get('/contacts', Contacts::class);
